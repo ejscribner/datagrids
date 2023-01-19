b97a52b3b6498b23459e728c8177ba0428d96896
Bartek Słysz <bartekslysz@poczta.fm>
2023-01-19 18:45:36 +0100

    GG-146: Add Tailwind CSS to the project (#9)
    * GG-146: Install tailwind & inject it into the project
    
    * GG-146: Restore already existing tailwind config
    
    * GG-146: Copy the color palette and default styles
    
    * GG-146: Dispose of precomputed aspect-ratio

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
